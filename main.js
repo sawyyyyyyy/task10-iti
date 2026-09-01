e350334b6ab5693f85accba058459a5ea9de70bf
sawyyyyyyy <snaasawy9292@gmail.com>
2026-09-01 23:26:00 +0300

    push js file

--- main.js
+++ main.js
@@ -1,17 +1,105 @@
-//var num = Number(window.prompt("write num"));
-//console.log(num);
+/* Q1
+var num = Number(window.prompt("write num"));
+console.log(num);*/
 
 
-//var num = Number(window.prompt("write num"));
-//if (num%3==0 && num%4==0){
-   // console.log("yes");
-//}
-//else{
-  //  console.log("no");
-//}
+/* Q2
+var num = Number(window.prompt("write num"));
+if (num%3==0 && num%4==0){
+    console.log("yes");
+}
+else{
+    console.log("no");
+}*/
 
 
-
+/* Q3
 var num1 = Number(window.prompt("write first num"));
 var num2 = Number(window.prompt("write second num"));
-console.log(Math.max(num1,num2));
+console.log(Math.max(num1,num2));*/
+
+
+/*Q4
+var num = Number(window.prompt("write num"));
+if (num<0){
+    console.log("negative");
+}
+else{
+    console.log("positive");
+} */
+
+
+ /*Q5   
+var num1 = Number(window.prompt("write first num"));
+var num2 = Number(window.prompt("write second num"));
+var num3 = Number(window.prompt("write third num"));
+console.log("max number:" + Math.max(num1,num2,num3));
+console.log("min number:" + Math.min(num1,num2,num3));
+*/
+
+
+/*Q6
+var num = Number(window.prompt("write num"));
+if (num%2==0){
+    console.log("even");
+}
+else{
+    console.log("odd");
+}
+*/
+
+/*Q7
+var char =window.prompt("write character") ;
+if (char===a || char===e || char===i || char===o || char===u){
+    console.log("vowel");
+}
+else{
+    console.log("consontant");
+}
+    */
+
+
+/*Q8
+var num = Number(window.prompt("write num"));
+for (let i=1 ; i<=num ; i++){
+    console.log(i);
+}
+*/
+
+
+/*Q9
+var num = Number(window.prompt("write num"));
+for (let i=1 ; i<=12 ; i++){
+    console.log(i*num);
+}
+    */
+
+
+/*Q10
+var num = Number(window.prompt("write num"));
+for (let i=1 ; i<=num ; i++){
+   if(i%2 ==0){
+    console.log(i); }
+}
+    */
+
+
+/*Q11
+var num1 = Number(window.prompt("write first num"));
+var num2 = Number(window.prompt("write second num"));
+console.log(Math.pow(num1,num2));
+*/
+
+/*Q12
+var sub1 = Number(window.prompt("write the mark of first subject"));
+var sub2 = Number(window.prompt("write the mark of second subject"));
+var sub3 = Number(window.prompt("write the mark of third subject"));
+var sub4 = Number(window.prompt("write the mark of forth subject"));
+var sub5 = Number(window.prompt("write the mark of fifth subject"));
+var total= sub1+sub2+sub3+sub4+sub5;
+var average= total/5;
+var percentage= (total/500) *100;
+console.log("total marks = " + total);
+console.log("avrage marks = " + average );
+console.log("percentage = " + percentage + "%" );
+*/
