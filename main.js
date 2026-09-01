07ba94f6138f78bec983b9d76797b1defc4e088d
sawyyyyyyy <snaasawy9292@gmail.com>
2026-09-01 15:27:08 +0300

    push js file

---
+++ main.js
@@ -0,0 +1,17 @@
+//var num = Number(window.prompt("write num"));
+//console.log(num);
+
+
+//var num = Number(window.prompt("write num"));
+//if (num%3==0 && num%4==0){
+   // console.log("yes");
+//}
+//else{
+  //  console.log("no");
+//}
+
+
+
+var num1 = Number(window.prompt("write first num"));
+var num2 = Number(window.prompt("write second num"));
+console.log(Math.max(num1,num2));
